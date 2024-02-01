8e3de41f527665a7787fa05479af63ea2982017a
Rishi Deorukhkar <117723599+yoshi-doo@users.noreply.github.com>
2024-02-01 08:43:22 +0100

    feat: change percentage

--- src/utils/Percentage.sol
+++ src/utils/Percentage.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13 .0;
 
 library Percentages {
-    function percentage(uint256 amount, uint256 bps) public pure returns (uint256) {
+    function percentage(uint256 amount, uint256 bps) internal pure returns (uint256) {
         require((amount * bps) >= 10_000, "Values round off to zero");
         return (amount * bps) / 10_000;
     }
